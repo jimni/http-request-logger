bad222497fa695bc52581f153d285fd055078dfb
jimni <jim@MacBook-Pro-Anton-2.local>
2017-05-08 01:03:42 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # http-request-logger
-Simple "200 ok, log to stdout" python server
+Simple "200 ok, log to stdout" python http server.
+
+Intentionally python 2 only. You can run this on practically any *nix machine without sudo or any specific access.
+
+Don't forget to set preferred port number.
+
+View requests:  `python http-server.py`.
+Log requests: `python http-server.py > log.txt`
---
+++ http-server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# Reflects the requests from HTTP methods GET, POST, PUT, and DELETE
+# Written by Nathan Hamiel (2010, https://gist.github.com/huyng/814831)
+# Modified by jimni
+
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+class RequestHandler(BaseHTTPRequestHandler):
+        
+    def do_POST(self):
+
+        print(self.log_date_time_string() + ' from '
+              + str(self.client_address[0]) + ':'
+              + str(self.client_address[1]))
+        print("----- Request Start ----->\n")
+        print(self.command + ' ' + self.path + ' ' + self.request_version)
+
+        content_length = self.headers.getheaders('content-length')
+        length = int(content_length[0]) if content_length else 0
+        
+        print(self.headers)
+        if length > 0: print(self.rfile.read(length))
+        print("<----- Request End -----\n")
+        
+        self.send_response(200)
+
+    do_GET = do_POST
+    do_PUT = do_POST
+    do_DELETE = do_GET
+        
+def main():
+    port = 8080
+    print('Listening all interfaces on port %s\n' % port)
+    server = HTTPServer(('', port), RequestHandler)
+    server.serve_forever()
+
+        
+if __name__ == "__main__":
+    main()
